1f17b18091b7f5b865de984ea7310f50e11b7e71
mateusborgessena9 <mateusborgessena9@gmail.com>
2025-11-28 11:42:35 -0300

    a

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
-def home():
-    return render_template('index.html')
+@app.route('/sobre')
+def sobre():
+    return render_template('sobre.html')
 
 
 if __name__ == '__main__':
