1e8655bdca1503e8c0b3561ec19c2f27c3afbf21
mateusborgessena9 <mateusborgessena9@gmail.com>
2025-11-28 11:30:41 -0300

    a

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_templates
 
 
 app = Flask(__name__)
@@ -6,8 +6,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_templates('index.html')
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=80, debug=True)
+    app.run(debug=True)
