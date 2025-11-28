6a6312fe38d458f2aed512779b84ca41f73f165b
mateusborgessena9 <mateusborgessena9@gmail.com>
2025-11-28 11:31:11 -0300

    a

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_templates
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_templates('index.html')
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
