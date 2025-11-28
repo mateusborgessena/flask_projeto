43afb46a113280187b54026d93a3684a96979546
mateusborgessena9 <mateusborgessena9@gmail.com>
2025-11-28 12:02:23 -0300

    u

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
 from flask import Flask, render_template
 
-
 app = Flask(__name__)
-
 
 @app.route('/sobre')
 def sobre():
-    return render_template("sobre.html")
-
+    return render_template('sobre.html')
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=80, debug=True)
+    app.run(debug=True)
