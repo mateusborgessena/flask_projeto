e3906816693829f537216b2f69329e6849049656
mateusborgessena9 <mateusborgessena9@gmail.com>
2025-12-05 10:54:22 -0300

    a

--- professor_service.py
+++ professor_service.py
@@ -1,20 +1,22 @@
-class  professor:
+class  Professor:
     def __init__(self, id, nome, matricula):
         self.id = id
         self.nome = nome
         self.matricula = matricula
+
 class ProfessorService:
     def __init__(self):
         self.lista = []
         self.proximo_id = 1
+
         self.adicionar("Ana Braga", "2025137")
         self.adicionar("Jorge Batista", "2029510")
         self.adicionar("Maria Costa", "2023141")
 
     def adicionar(self, nome, matricula):
         id = self.proximo_id
-        professor = professor(id, nome, matricula)
-        self.lista.append(professor)
+        novo_professor = Professor(id, nome, matricula)
+        self.lista.append(novo_professor)
         self.proximo_id += 1
 
     def listar(self):
