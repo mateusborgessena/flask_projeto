2b27b33c5490065e52d329f1c7bcdef2d49572d5
mateusborgessena9 <mateusborgessena9@gmail.com>
2025-12-05 10:52:09 -0300

    p

--- aluno_service.py
+++ aluno_service.py
@@ -1,20 +1,23 @@
-class  Aluno:
+class Aluno:
     def __init__(self, id, nome, matricula):
         self.id = id
         self.nome = nome
         self.matricula = matricula
+
+
 class AlunoService:
     def __init__(self):
         self.lista = []
         self.proximo_id = 1
+
+
         self.adicionar("Maria Silva", "2023801")
         self.adicionar("João Pereira", "2023802")
         self.adicionar("Ana Costa", "2023003")
 
     def adicionar(self, nome, matricula):
-        id = self.proximo_id
-        aluno = aluno(id, nome, matricula)
-        self.lista.append(aluno)
+        novo_aluno = Aluno(self.proximo_id, nome, matricula)
+        self.lista.append(novo_aluno)
         self.proximo_id += 1
 
     def listar(self):
