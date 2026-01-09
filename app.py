f2217ce952591de0c02080dba75bacb9a2af4dc3
mateusborgessena9 <mateusborgessena9@gmail.com>
2026-01-09 11:28:06 -0300

    a

--- app.py
+++ app.py
@@ -86,9 +86,9 @@ def salvar_professor():
     disciplina = request.form.get("disciplina")
     cpf = request.form.get("cpf")
     try:
-        professor_service.adicionar(nome, cpf)
+        professor_service.adicionar(nome, cpf, disciplina)
     except  Exception as e:
-        professor = Professor('',nome,cpf)
+        professor = Professor('',nome,cpf, disciplina)
         return render_template("professor/form.html",professor=professor, erro=str(e))
 
 @app.route("/professor/editar/<int:id>")
