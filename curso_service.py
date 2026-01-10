11c729f757a057a191acaec94a60484dbe1e3657
mateusborgessena9 <mateusborgessena9@gmail.com>
2026-01-10 08:04:46 -0300

    a

--- curso_service.py
+++ curso_service.py
@@ -1,7 +1,7 @@
 class  Curso:
-    def __init__(self, id, nome, nivel):
+    def __init__(self, id, curso, nivel):
         self.id = id
-        self.nome = nome
+        self.curso = curso
         self.nivel = nivel
 
 class CursoService:
@@ -9,14 +9,14 @@ class CursoService:
         self.lista = []
         self.proximo_id = 1
 
-    def adicionar(self, nome, nivel):
-        if not nome or not nivel:
-            raise Exception("Nome e nivel são obrigatórios")
+    def adicionar(self, curso, nivel):
+        if not curso or not nivel:
+            raise Exception("Curso e nivel são obrigatórios")
         for curso in self.lista:
-            if curso.nome == nome:
-                raise Exception("NOME já existe")
+            if curso.curso == curso:
+                raise Exception("Curso já existe")
         id = self.proximo_id
-        curso = Curso(id, nome, nivel)
+        curso = Curso(id, curso, nivel)
         self.lista.append(curso)
         self.proximo_id += 1
 
