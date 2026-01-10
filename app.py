a6aae560c08c4e7b46a7ea460f002ca0893f965d
mateusborgessena9 <mateusborgessena9@gmail.com>
2026-01-10 08:19:44 -0300

    a

--- app.py
+++ app.py
@@ -99,7 +99,7 @@ def editar_professor(id):
     cpf = request.form.get("cpf")
     disciplina = request.form.get("disciplina")
     try:
-        professor_service.editar(nome, cpf, disciplina)
+        professor_service.atualizar(nome, cpf, disciplina)
     except Exception as e:
         professor = Professor('', nome, cpf, disciplina)
         return render_template("professor/form.html",professor=professor,erro=str(e))
